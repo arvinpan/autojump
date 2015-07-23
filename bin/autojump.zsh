32ba3948954870b6e749eaae97a0b5a545beb69f
Léo Peltier <lpeltier@universcine.com>
2015-07-23 12:17:19 +0200

    Add --child support to shell scripts.

--- bin/autojump.zsh
+++ bin/autojump.zsh
@@ -70,10 +70,10 @@ j() {
 # jump to child directory (subdirectory of current path)
 jc() {
     if [[ ${1} == -* ]] && [[ ${1} != "--" ]]; then
-        autojump ${@}
+        autojump --child ${@}
         return
     else
-        j $(pwd) ${@}
+        j ${@} --child
     fi
 }
 
@@ -114,9 +114,9 @@ jo() {
 # open autojump results (child directory) in file browser
 jco() {
     if [[ ${1} == -* ]] && [[ ${1} != "--" ]]; then
-        autojump ${@}
+        autojump --child ${@}
         return
     else
-        jo $(pwd) ${@}
+        jo ${@} --child
     fi
 }
